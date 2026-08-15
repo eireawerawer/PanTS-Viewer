9234c670ca0774a65a55150d6fc3e68b5643c035
harleensachdev <blackberezz@gmail.com>
2026-08-15 23:45:22 +0800

    update toolbar spacing

--- PanTS-Demo/src/components/viewer/AnnotationToolbar.tsx
+++ PanTS-Demo/src/components/viewer/AnnotationToolbar.tsx
@@ -1024,7 +1024,7 @@ export default function AnnotationToolbar({
 			aria-orientation="horizontal"
 		>
 			<div ref={dockContentRef} style={{ display: "flex", alignItems: "center", width: "100%" }}>
-			<div style={{ display: "flex", alignItems: "center", flex: 1, minWidth: 0, gap: 20}}>
+			<div style={{ display: "flex", alignItems: "center", flex: 1, minWidth: 0, gap: 13}}>
 				{TOOL_DEFS.map(({ id, label, Icon, description }) => {
 					// Only equip-and-use tools (paint/erase/scissors/level tracing)
 					// get a settings arrow; other tools open settings on icon click.
